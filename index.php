63b25ad36016a539d68c473b7b08dc00257fdd44
Anton Zekeriev Rodin <antonzekeriev@gmail.com>
2013-11-27 18:30:52 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8" />
+		<title></title>
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<meta name="robots" content="all" />
+		<link href="" rel="stylesheet" >
+        </head>
+<body>
+    
+<header></header>
+
+<section>
+</section>
+
+<aside></aside>
+<footer></footer>
+
+</body>
+</html>
