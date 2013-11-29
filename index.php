fec239e8f91adc6826afffed7cda560862db4915
Anton Zekeriev Rodin <antonzekeriev@gmail.com>
2013-11-29 15:44:55 +0100

    Install Bootstrap css

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html lang="es">
-	<head>
-		<meta charset="UTF-8" />
-		<title></title>
-		<meta name="keywords" content="" />
-		<meta name="description" content="" />
-		<meta name="robots" content="all" />
-		<link href="" rel="stylesheet" >
-        </head>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" >
+<?php wp_head(); ?>
+</head>
 <body>
-    
 <header></header>
 
 <section>
