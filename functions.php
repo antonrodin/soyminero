d19ab9cc2440c647cab26aa4071b3069596deb8e
Anton <antonzekeriev@gmail.com>
2013-12-05 21:42:57 +0100

    Try to ADD custom posts

--- functions.php
+++ functions.php
@@ -44,4 +44,91 @@ function footer_widgets_init() {
 }
 add_action( 'widgets_init', 'footer_widgets_init' );
 
+/** Last Videos
+ *  Register a Custom Video Post's
+ */
+function video_custom_post_init()   {  
+  $labels = array(  
+    'name' => _x('Videos', 'post type general name'),  
+    'singular_name' => _x('Video', 'post type singular name'),  
+    'add_new' => _x('Add New', 'video'),  
+    'add_new_item' => __('Add New Video'),  
+    'edit_item' => __('Edit Video'),  
+    'new_item' => __('New Video'),  
+    'view_item' => __('View Video'),  
+    'search_items' => __('Search Videos'),  
+    'not_found' =>  __('No videos found'),  
+    'not_found_in_trash' => __('No videos found in Trash'),  
+    'parent_item_colon' => '',  
+    'menu_name' => 'Videos'  
+  );
+  
+  $args = array(  
+    'labels' => $labels,  
+    'public' => true,  
+    'publicly_queryable' => true,  
+    'show_ui' => true,  
+    'show_in_menu' => true,  
+    'query_var' => true,  
+    'rewrite' => true,  
+    'capability_type' => 'post',  
+    'has_archive' => true,  
+    'hierarchical' => false,  
+    'menu_position' => null,  
+    'supports' => array('title','editor','author','thumbnail','excerpt','comments')  
+  );  
+   
+  register_post_type('video',$args); 
+}
+add_action('init', 'video_custom_post_init');
+
+/**
+ * Update messages for custom video post
+ */ 
+function videos_updated_messages( $messages ) {  
+  global $post, $post_ID;  
+  
+  $messages['video'] = array(  
+    0 => '', // Unused. Messages start at index 1.  
+    1 => sprintf( __('Video updated. <a href="%s">View video</a>'), esc_url( get_permalink($post_ID) ) ),  
+    2 => __('Custom field updated.'),  
+    3 => __('Custom field deleted.'),  
+    4 => __('Video updated.'),  
+    /* translators: %s: date and time of the revision */  
+    5 => isset($_GET['revision']) ? sprintf( __('Video restored to revision from %s'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,  
+    6 => sprintf( __('Video published. <a href="%s">View video</a>'), esc_url( get_permalink($post_ID) ) ),  
+    7 => __('Video saved.'),  
+    8 => sprintf( __('Video submitted. <a target="_blank" href="%s">Preview video</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),  
+    9 => sprintf( __('Project scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview project</a>'),  
+      // translators: Publish box date format, see http://php.net/date  
+      date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),  
+    10 => sprintf( __('Video draft updated. <a target="_blank" href="%s">Video project</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),  
+  );  
+  
+  return $messages;  
+}
+add_filter('post_updated_messages', 'videos_updated_messages'); 
+
+// Initialize New Taxonomy Labels  
+$labels = array(  
+    'name' => _x( 'Tags', 'taxonomy general name' ),  
+    'singular_name' => _x( 'Tag', 'taxonomy singular name' ),  
+    'search_items' =>  __( 'Search Types' ),  
+    'all_items' => __( 'All Tags' ),  
+    'parent_item' => __( 'Parent Tag' ),  
+    'parent_item_colon' => __( 'Parent Tag:' ),  
+    'edit_item' => __( 'Edit Tags' ),  
+    'update_item' => __( 'Update Tag' ),  
+    'add_new_item' => __( 'Add New Tag' ),  
+    'new_item_name' => __( 'New Tag Name' ),  
+);  
+  
+register_taxonomy('tag', array('video'), array(  
+    'hierarchical' => false,  
+    'labels' => $labels,  
+    'show_ui' => true,  
+    'query_var' => true,  
+    'rewrite' => array( 'slug' => 'tag' ),  
+));  
+
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,86 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-<?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
+<div class="row">
+    
+    <!-- Custom Page about Bitcoins -->
+    <div class="col-lg-6">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+    <?php endwhile; else: ?>
+    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+    <?php endif; ?>
+    </div>
+
+    <!-- Video Blog -->
+    <div class="col-lg-6">
+        <?php $loop = new WP_Query(array('post_type' => 'video', 'posts_per_page' => -1));  
+            $count =0;  
+        ?>
+        <?php if($loop) { ?>  
+        <ul id="folio" class="grid">  
+            <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+            <li class="item-<?php the_ID() ?> <?php if(++$count%4==0) echo 'rightmost'?> ">  
+            <div class="image">  
+                <span>  
+                    <a href="<?php the_permalink() ?>">  
+                        <?php  
+                            if(has_post_thumbnail()){  
+                                the_post_thumbnail('thumbnail');  
+                            }  
+                        ?>  
+                    </a>  
+                </span>  
+                <a href="<?php the_permalink() ?>" class="link">View Details</a>  
+            </div>
+                <div class="content">  
+                    <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>  
+                    <span class="tags">  
+                        <?php  
+                            // Fetching the tag names with respect to the post and displaying them  
+                            $args = array('orderby' => 'name', 'order' => 'ASC', 'fields' => 'names');  
+                            echo implode(wp_get_object_terms( $post->ID, 'tag', $args),', ');  
+                        ?>  
+                    </span>  
+                    <p>  
+                        <?php  
+                            // Using custom excerpt function to fetch the excerpt  
+                            folio_excerpt('folio_excerpt_length','folio_excerpt_more');  
+                         ?>  
+                    </p>  
+                </div>  
+                <div class="clear"></div>  
+                </li>  
+                    <?php endwhile; ?>  
+                </ul>  
+            <?php } ?>  
+            <?php wp_reset_query(); ?>
+            <?php
+            // Adding Variable Excerpt Length
+            function folio_excerpt_length($length) {
+                return 80;
+            }
+            function folio_excerpt_more($more) {
+                    return ' ... <span class="excerpt_more"><a href="'.get_permalink().'">Read more</a></span>';
+            }
+            function folio_excerpt($length_callback='', $more_callback='') {
+                global $post;
+                if(function_exists($length_callback)){
+                    add_filter('excerpt_length', $length_callback);
+                }
+                if(function_exists($more_callback)){
+                    add_filter('excerpt_more', $more_callback);
+                }
+                $output = get_the_excerpt();
+                $output = apply_filters('wptexturize', $output);
+                $output = apply_filters('convert_chars', $output);
+                $output = '<p>'.$output.'</p>';
+                echo $output;
+            }
+            ?>
+        <!-- End Video Blog -->
+    </div>
+    
+</div>
 
 <?php get_footer(); ?>
