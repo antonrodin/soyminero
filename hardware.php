ba4b27a5e4ae483897d1cd0868ad3c27a5a4b347
Anton <antonzekeriev@gmail.com>
2013-12-08 11:59:38 +0100

    Add Hardware Themplate page

---
+++ hardware.php
@@ -0,0 +1,19 @@
+<?php 
+    /* 
+    * Template Name: Hardware 
+    */
+
+    get_header(); 
+?>
+
+<div class="row">
+  
+    <!-- Video Blog -->
+    <div class="col-lg-6">
+    </div>
+    <div class="col-lg-6">
+    </div>
+    
+</div>
+
+<?php get_footer(); ?>
