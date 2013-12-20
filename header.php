cc9d397f36aebeb3e649ed8b7736e3a5e9e019cc
Anton <antonzekeriev@gmail.com>
2013-12-20 18:41:03 +0100

    Add favicons

--- header.php
+++ header.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+    
+<!-- Begin Favicons -->
+<link rel="shortcut icon" href="<?php bloginfo("template_url"); ?>/img/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="<?php bloginfo("template_url"); ?>/img/apple-touch-icon.png" />
+<link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo("template_url"); ?>/img/apple-touch-icon-57x57.png" />
+<link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo("template_url"); ?>/img/apple-touch-icon-60x60.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo("template_url"); ?>/img/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo("template_url"); ?>/img/apple-touch-icon-76x76.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo("template_url"); ?>/img/apple-touch-icon-114x114.png" />
+<link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo("template_url"); ?>/img/apple-touch-icon-120x120.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo("template_url"); ?>/img/apple-touch-icon-144x144.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo("template_url"); ?>/img/apple-touch-icon-152x152.png" />
+<!-- End Favicons -->
+
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
